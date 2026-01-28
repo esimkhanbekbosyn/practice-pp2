8108974da604fcc9629434fd5985601553d12181
esimkhanbekbosyn <esimkhanbekbosin@gmail.com>
2026-01-28 23:10:24 +0500

    type

---
+++ practice1/pythonbasic/5.py
@@ -0,0 +1,18 @@
+x = "Hello World"	#str	
+print(x,'str')
+x = 20	#int	
+print(x,'int')
+x = 20.5	#float	
+print(x,'float')
+x = ["apple", "banana", "cherry"]	#list	
+print(x,'list')
+x = ("apple", "banana", "cherry")	#tuple	
+print(x,'tuple')
+x = range(6)	#range	
+print(x,'range')
+x = {"name" : "John", "age" : 36}	#dict	
+print(x,'dict')
+x = {"apple", "banana", "cherry"}	#set	
+print(x,'set')
+x = True	#bool
+print(x,'bool')
